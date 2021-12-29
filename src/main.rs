3e0d3e90510e6795afa02580eb1994cf79856e8e
Brian Hicks <brian@brianthicks.com>
2021-12-29 16:05:11 -0600

    get the wiring all taken care of

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Opts {
+    #[clap(long)]
+    command: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = try_main() {
+        eprintln!("{:#?}", err);
+        std::process::exit(1);
+    }
+}
+
+fn try_main() -> Result<()> {
+    let opts = Opts::parse();
+
+    println!("{:#?}", opts);
+
+    Ok(())
 }
