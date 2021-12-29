d0cdd595b2b48236e67ff24f1a30909e67624f9a
Brian Hicks <brian@brianthicks.com>
2021-12-29 15:57:06 -0600

    add clippy

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        devShell =
-          pkgs.mkShell { packages = [ pkgs.rustc pkgs.cargo pkgs.libiconv ]; };
+        devShell = pkgs.mkShell {
+          packages = [ pkgs.rustc pkgs.cargo pkgs.clippy pkgs.libiconv ];
+        };
       });
 }
