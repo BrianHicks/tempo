ab4a03f0e11de8527d67f9f8bc4e201d6792de7c
Brian Hicks <brian@brianthicks.com>
2021-12-29 15:42:07 -0600

    start a flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell { packages = [ pkgs.rustc pkgs.cargo ]; };
+      });
+}
