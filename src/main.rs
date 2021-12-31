1271da9db776849ef32188f4dfa3dafafe7b9b3e
Brian Hicks <brian@brianthicks.com>
2021-12-31 06:11:23 -0600

    parse duration values

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
+use chrono::Duration;
 use clap::Parser;
-
-mod pid;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
@@ -13,6 +12,26 @@ struct Opts {
 #[derive(Parser, Debug)]
 enum Command {
     Current,
+    /// Add a new item to the store
+    Add(AddCommand),
+}
+
+#[derive(Parser, Debug)]
+struct AddCommand {
+    /// What category does this item belong to?
+    category: String,
+
+    /// Text of the item to add
+    text: String,
+
+    /// Initial guess on cadence. Don't worry about this being incorrect; we'll
+    /// find the right value over time!
+    #[clap(short, long, default_value = "1d", parse(try_from_str = parse_duration))]
+    cadence: Duration,
+
+    /// When should this next be scheduled?
+    #[clap(short, long)]
+    next: Option<String>,
 }
 
 fn main() {
@@ -25,9 +44,85 @@ fn main() {
 fn try_main() -> Result<()> {
     let opts = Opts::parse();
 
-    let command = opts.command.unwrap_or(Command::Current);
-
-    println!("{:#?}", command);
+    println!("{:#?}", opts);
 
     Ok(())
+}
+
+// Parse a simple duration string: a number followed by a unit of hours (h),
+// days (d), or weeks (w). Months and years are not supported due to varying
+// length in those units.
+fn parse_duration(input: &str) -> Result<Duration> {
+    let mut digits_offset = 0;
+    let mut tag = None;
+
+    for c in input.chars() {
+        // meaning: we've already assigned a value to tag, which means we're
+        // somewhere after it in the input string. There shouldn't be anything
+        // here, so we can just bail.
+        if tag != None {
+            bail!("got more input than I expected. I wanted to see a number and tag, like 10d.")
+        }
+        // meaning: we're done with the digits but haven't assigned a tag yet.
+        else if !c.is_numeric() {
+            tag = Some(c);
+        }
+        // meaning: we haven't completed scanning the digits yet.
+        else {
+            digits_offset += 1;
+        }
+    }
+
+    let digits = &input[0..digits_offset];
+    if digits.is_empty() {
+        bail!("I expected to see some numbers then a tag (like 1d), but I didn't see any numbers!");
+    }
+
+    let amount: i64 = digits
+        .parse()
+        .with_context(|| format!("could not parse a number from `{}`", digits))?;
+
+    let out = match tag {
+        Some('h') => Duration::hours(amount),
+        Some('d') => Duration::days(amount),
+        Some('w') => Duration::weeks(amount),
+        _ => bail!("expected to see a tag (h, d, w) after the amount"),
+    };
+
+    Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_duration_hours() {
+        assert_eq!(Duration::hours(1), parse_duration("1h").unwrap());
+    }
+
+    #[test]
+    fn parse_duration_multiple() {
+        assert_eq!(Duration::hours(24), parse_duration("24h").unwrap());
+    }
+
+    #[test]
+    fn parse_duration_days() {
+        assert_eq!(Duration::days(1), parse_duration("1d").unwrap());
+    }
+
+    #[test]
+    fn parse_duration_weeks() {
+        assert_eq!(Duration::weeks(1), parse_duration("1w").unwrap());
+    }
+
+    #[test]
+    fn parse_duration_extra() {
+        assert!(parse_duration("1dd").is_err());
+    }
+
+    #[test]
+    fn parse_duration_leading() {
+        assert!(parse_duration("d").is_err());
+    }
 }
