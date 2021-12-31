8407b3b95c3768e128de4b787f942392b5b6af8e
Brian Hicks <brian@brianthicks.com>
2021-12-31 06:15:03 -0600

    document supported units

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ struct AddCommand {
     text: String,
 
     /// Initial guess on cadence. Don't worry about this being incorrect; we'll
-    /// find the right value over time!
+    /// find the right value over time! Supported units: hours (h), days (d),
+    /// weeks (w), 30-day months (m), 365-day years (y)
     #[clap(short, long, default_value = "1d", parse(try_from_str = parse_duration))]
     cadence: Duration,
 
