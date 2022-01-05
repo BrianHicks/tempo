6fd9203585af53c4a97c937d8b0a3fa8f568c10a
Brian Hicks <brian@brianthicks.com>
2022-01-05 16:08:38 -0600

    add PID stuff to the table

--- migrations/V1__items.rs
+++ migrations/V1__items.rs
@@ -7,8 +7,17 @@ pub fn migration() -> String {
     m.create_table("items", |t| {
         t.add_column("id", types::primary());
         t.add_column("text", types::text());
+
+        // scheduling
         t.add_column("cadence", types::integer());
         t.add_column("next", types::datetime());
+
+        // PID
+        t.add_column("proportional_factor", types::float().default(1.5));
+        t.add_column("integral", types::float().default(0.0));
+        t.add_column("integral_factor", types::float().default(0.3));
+        t.add_column("last_error", types::float().default(0.0));
+        t.add_column("derivative_factor", types::float().default(0.1));
     });
 
     m.make::<Sqlite>()
