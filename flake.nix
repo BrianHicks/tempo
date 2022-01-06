d054ad0099c1ca0e640f6013f3fa05d54ad0a823
Brian Hicks <brian@brianthicks.com>
2022-01-06 10:57:40 -0600

    build the binary in the flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nmattia/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = naersk.lib."${system}";
+      in rec {
+        packages.tempo = naersk-lib.buildPackage {
+          root = ./.;
+
+          doCheck = true;
+          checkPhase = "cargo test";
+        };
+        defaultPackage = packages.tempo;
+        overlay = final: prev: { tempo = packages.tempo; };
+
         devShell = pkgs.mkShell {
           packages =
             [ pkgs.cargo pkgs.clippy pkgs.libiconv pkgs.rustc pkgs.rustfmt ];
