404c7af6ad60b6e3c24bf6382c409159ab70f500
Brian Hicks <brian@brianthicks.com>
2022-01-08 14:51:59 -0600

    limit and filter

--- src/cli/pull.rs
+++ src/cli/pull.rs
@@ -8,7 +8,7 @@ use rusqlite::Connection;
 pub struct Command {
     /// Only get up to this many items
     #[clap(long, short)]
-    limit: Option<u32>,
+    limit: Option<usize>,
 
     /// Only get items with these tags
     #[clap(long, short)]
@@ -25,13 +25,17 @@ struct Pulled {
 
 impl Command {
     pub fn run(&self, conn: &Connection, _format: Format) -> Result<()> {
+        // note to future explorers: the sqlite API doesn't let you use an array
+        // in a parameter, so we can't do like `WHERE tags.name IN ?`. So,
+        // we just pull everything and do the filtering locally. We're not
+        // dealing with enough data here for it to be a problem, though!
         let mut statement = conn
             .prepare(
                 "SELECT items.id, text, next, tags.name FROM items LEFT JOIN tags ON items.tag_id = tags.id ORDER BY next ASC",
             )
             .context("could not prepare query to pull items")?;
 
-        let pulled: Vec<Pulled> = statement
+        let unlimited = statement
             .query_map([], |row| {
                 Ok(Pulled {
                     id: row.get(0)?,
@@ -42,7 +46,15 @@ impl Command {
             })
             .context("could not pull next items")?
             .flatten()
-            .collect();
+            .filter(|item| match &item.tag {
+                Some(filter_tag) => self.tag.contains(filter_tag),
+                None => self.tag.is_empty(),
+            });
+
+        let pulled: Vec<Pulled> = match self.limit {
+            Some(limit) => unlimited.take(limit).collect(),
+            None => unlimited.collect(),
+        };
 
         println!("{:#?}", pulled);
 
