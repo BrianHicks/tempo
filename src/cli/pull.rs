6b03b1619ed9c47d040273b996fd69499f024e51
Brian Hicks <brian@brianthicks.com>
2022-01-09 06:07:43 -0600

    test the pull command

--- src/cli/pull.rs
+++ src/cli/pull.rs
@@ -17,7 +17,7 @@ pub struct Command {
     tag: Vec<String>,
 }
 
-#[derive(Debug, Serialize, Tabled)]
+#[derive(Debug, Serialize, Tabled, PartialEq)]
 struct Pulled {
     #[header("ID")]
     id: u64,
@@ -42,6 +42,23 @@ fn display_tag(tag_opt: &Option<String>) -> String {
 
 impl Command {
     pub fn run(&self, conn: &Connection, format: Format) -> Result<()> {
+        let pulled = self.items(conn)?;
+
+        match format {
+            Format::Human => println!(
+                "{}",
+                Table::new(pulled).with(tabled::Style::PSQL).to_string()
+            ),
+            Format::Json => println!(
+                "{}",
+                serde_json::to_string(&pulled).context("could not dump pulled items to JSON")?
+            ),
+        };
+
+        Ok(())
+    }
+
+    fn items(&self, conn: &Connection) -> Result<Vec<Pulled>> {
         // note to future explorers: the sqlite API doesn't let you use an array
         // in a parameter, so we can't do like `WHERE tags.name IN ?`. So,
         // we just pull everything and do the filtering locally. We're not
@@ -68,22 +85,156 @@ impl Command {
                 None => self.tag.is_empty(),
             });
 
-        let pulled: Vec<Pulled> = match self.limit {
+        Ok(match self.limit {
             Some(limit) => unlimited.take(limit).collect(),
             None => unlimited.collect(),
-        };
+        })
+    }
+}
 
-        match format {
-            Format::Human => println!(
-                "{}",
-                Table::new(pulled).with(tabled::Style::PSQL).to_string()
-            ),
-            Format::Json => println!(
-                "{}",
-                serde_json::to_string(&pulled).context("could not dump pulled items to JSON")?
-            ),
-        };
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cadence::Cadence;
+    use rusqlite::params;
 
-        Ok(())
+    fn conn() -> Connection {
+        let mut conn = Connection::open_in_memory().expect("couldn't open an in-memory database");
+        crate::db::migrations::runner()
+            .run(&mut conn)
+            .expect("couldn't migrate database");
+
+        conn
+    }
+
+    #[test]
+    fn not_due() {
+        let conn = conn();
+
+        conn.execute(
+            "INSERT INTO items (text, next, cadence) VALUES (?, ?, ?)",
+            params!["X", Utc::now() + Cadence::days(1), Cadence::days(1)],
+        )
+        .unwrap();
+
+        let command = Command::try_parse_from(&["pull"]).unwrap();
+        let items = command.items(&conn).unwrap();
+
+        assert!(items.is_empty());
+    }
+
+    #[test]
+    fn due() {
+        let conn = conn();
+
+        let cadence = Cadence::days(1);
+        let next = Utc::now() - cadence;
+        conn.execute(
+            "INSERT INTO items (text, next, cadence) VALUES (?, ?, ?)",
+            params!["X", next, cadence],
+        )
+        .unwrap();
+
+        let command = Command::try_parse_from(&["pull"]).unwrap();
+        let items = command.items(&conn).unwrap();
+
+        assert_eq!(
+            vec![Pulled {
+                id: 1,
+                text: "X".into(),
+                next,
+                tag: None
+            }],
+            items
+        );
+    }
+
+    #[test]
+    fn not_matching_tag() {
+        let conn = conn();
+
+        let cadence = Cadence::days(1);
+        let next = Utc::now() - cadence;
+        conn.execute(
+            "INSERT INTO items (text, next, cadence) VALUES (?, ?, ?)",
+            params!["X", next, cadence],
+        )
+        .unwrap();
+
+        let command = Command::try_parse_from(&["pull", "--tag", "x"]).unwrap();
+        let items = command.items(&conn).unwrap();
+
+        assert!(items.is_empty());
+    }
+
+    #[test]
+    fn matching_tag() {
+        let conn = conn();
+
+        let tag_name = "tag";
+        let tag_id: u64 = conn
+            .query_row(
+                "INSERT INTO tags (name) VALUES (?) RETURNING id",
+                [tag_name],
+                |row| row.get(0),
+            )
+            .unwrap();
+
+        let cadence = Cadence::days(1);
+        let next = Utc::now() - cadence;
+        conn.execute(
+            "INSERT INTO items (text, next, cadence, tag_id) VALUES (?, ?, ?, ?)",
+            params!["X", next, cadence, tag_id],
+        )
+        .unwrap();
+
+        let command = Command::try_parse_from(&["pull", "--tag", tag_name]).unwrap();
+        let items = command.items(&conn).unwrap();
+
+        assert_eq!(
+            vec![Pulled {
+                id: 1,
+                text: "X".into(),
+                next,
+                tag: Some(tag_name.into()),
+            }],
+            items
+        );
+    }
+
+    #[test]
+    fn limit() {
+        let conn = conn();
+
+        let later = Cadence::days(1);
+        let earlier = Cadence::days(2);
+        let now = Utc::now();
+
+        let due_earlier = now - earlier;
+        conn.execute(
+            "INSERT INTO items (text, next, cadence) VALUES (?, ?, ?)",
+            params!["Due Earlier", due_earlier, earlier],
+        )
+        .unwrap();
+
+        let due_later = now - later;
+        conn.execute(
+            "INSERT INTO items (text, next, cadence) VALUES (?, ?, ?)",
+            params!["Due Later", due_later, later],
+        )
+        .unwrap();
+
+        let command = Command::try_parse_from(&["pull", "--limit", "1"]).unwrap();
+        let items = command.items(&conn).unwrap();
+
+        assert_eq!(
+            vec![Pulled {
+                id: 1,
+                text: "Due Earlier".into(),
+                next: due_earlier,
+                tag: None,
+            }],
+            items
+        );
     }
 }
