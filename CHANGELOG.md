fa998b4cf64100a866ec9a8677e304cfba414966
Brian Hicks <brian@brianthicks.com>
2022-01-12 16:58:35 -0600

    parse datetimes in local TZ and convert to UTC immediately

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 - "pull" is renamed "ready" (but `tempo pull` will still work, at least for now!)
 - added ID to "ready" output
 - added "all" command to get all the items in the store
+- parsed dates are now interpeted using the local time zone instead of UTC
 
 # 1.0.0
 
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,12 +6,14 @@ pub mod finish;
 pub mod ready;
 
 use anyhow::{Context, Result};
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Local, TimeZone, Utc};
 
 fn parse_utc_datetime(input: &str) -> Result<DateTime<Utc>> {
-    Utc.datetime_from_str(input, "%Y-%m-%dT%H:%M:%S")
-        .or_else(|_| Utc.datetime_from_str(&format!("{}T00:00:00", input), "%Y-%m-%dT%H:%M:%S"))
-        .context("couldn't parse a date")
+    Ok(Local
+        .datetime_from_str(input, "%Y-%m-%dT%H:%M:%S")
+        .or_else(|_| Local.datetime_from_str(&format!("{}T00:00:00", input), "%Y-%m-%dT%H:%M:%S"))
+        .context("couldn't parse a date")?
+        .with_timezone(&Utc))
 }
 
 #[cfg(test)]
@@ -25,7 +27,7 @@ mod test {
         #[test]
         fn date() {
             assert_eq!(
-                Utc.ymd(2022, 1, 1).and_hms(0, 0, 0),
+                Local.ymd(2022, 1, 1).and_hms(0, 0, 0),
                 parse_utc_datetime("2022-01-01").unwrap()
             );
         }
@@ -33,7 +35,7 @@ mod test {
         #[test]
         fn datetime() {
             assert_eq!(
-                Utc.ymd(2022, 1, 1).and_hms(3, 2, 1),
+                Local.ymd(2022, 1, 1).and_hms(3, 2, 1),
                 parse_utc_datetime("2022-01-01T03:02:01").unwrap()
             );
         }
